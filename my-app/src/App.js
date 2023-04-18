64724aacd0fa8f661b24310661c57d652317437d
unknown <aniketghormare62@gmail.com>
2023-04-18 15:43:47 +0530

    new commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import AllRoutes from './components/AllRoutes';
 
 function App() {
   return (
     <div className="App">
      <h1>Portfolio</h1>
+     <Navbar/>
+     <AllRoutes/>
     </div>
   );
 }
---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <div className='nav-menu'>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/contact">Contact</Link>
+    
+    </div>
+  )
+}
+
+export default Navbar
